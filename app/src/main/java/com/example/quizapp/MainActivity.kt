d04fd17763e43e50841dcad93aebaeeb17bbdcb9
Anush Shand <anush.shand@byjus.com>
2022-08-29 11:56:28 +0530

    Image Feature has been Added

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_main)
+
     }
+
 }
