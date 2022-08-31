3ebc6aa9478c501102e539539406f56e4447ab10
Anush Shand <anush.shand@byjus.com>
2022-08-31 15:12:09 +0530

    DataStore added

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatDelegate
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/quizapp/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/quizapp/screens/title/TitleFragment.kt
@@ -1,27 +1,35 @@
 package com.example.quizapp.screens.title
 
+import android.app.UiModeManager
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.quizapp.R
+import com.example.quizapp.database.SettingsDataStore
 import com.example.quizapp.databinding.FragmentTitleBinding
+import kotlinx.coroutines.launch
 
 
 class TitleFragment : Fragment() {
     private lateinit var binding:FragmentTitleBinding
     private lateinit var viewModel: SharedNameViewModel
+    private lateinit var SettingsDataStore: SettingsDataStore
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
         (activity as AppCompatActivity).supportActionBar?.title = "QuizApp"
+        SettingsDataStore = SettingsDataStore(requireContext())
         viewModel=ViewModelProvider(requireActivity())[SharedNameViewModel::class.java]
         binding= DataBindingUtil.inflate(inflater, R.layout.fragment_title,container,false)
         binding.playButton.setOnClickListener{playGame()}
@@ -39,8 +47,18 @@ class TitleFragment : Fragment() {
                 }
             }
         })
+        //Data Store
+        binding.checkRemember.setOnClickListener{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            if(binding.checkRemember.isChecked)
+                lifecycleScope.launch {SettingsDataStore.storeName(requireContext(),binding.editName.text.toString())}
+            else
+                lifecycleScope.launch {SettingsDataStore.storeName(requireContext(),"")}
+        }
+        SettingsDataStore.nameFlow.asLiveData().observe(viewLifecycleOwner) {
+            binding.editName.setText(it.toString())
+        }
         return binding.root
-
     }
     private fun playGame()
     {
